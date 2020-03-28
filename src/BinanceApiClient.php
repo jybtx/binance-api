acec0ecb92c6abeb06407b558a1df7a285120ba2
root <2574034626@qq.com>
2020-03-28 08:43:45 +0800

    添加注释

--- src/BinanceApiClient.php
+++ src/BinanceApiClient.php
@@ -5,7 +5,7 @@ namespace Jybtx\BinanceApi;
 class BinanceApiClient
 {
 	
-	protected $secret;
+    protected $secret;
     protected $key;
     protected $base;
     public $btc_value = 0.00;
@@ -13,40 +13,97 @@ class BinanceApiClient
     public  function __construct($key,$secret,$base)
     {
         $this->secret = $secret;
-        $this->key = $key;
-        $this->base = $base;
+        $this->key    = $key;
+        $this->base   = $base;
     }
 
+    /**
+     * [buy description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol   [description]
+     * @param  [type]     $quantity [description]
+     * @param  [type]     $price    [description]
+     * @param  string     $type     [description]
+     * @return [type]               [description]
+     */
     public function buy($symbol, $quantity, $price, $type = "LIMIT")
     {
         return $this->order("BUY", $symbol, $quantity, $price, $type);
     }
-
+    /**
+     * [sell description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol   [description]
+     * @param  [type]     $quantity [description]
+     * @param  [type]     $price    [description]
+     * @param  string     $type     [description]
+     * @return [type]               [description]
+     */
     public function sell($symbol, $quantity, $price, $type = "LIMIT")
     {
         return $this->order("SELL", $symbol, $quantity, $price, $type);
     }
 
+    /**
+     * [cancel description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol  [description]
+     * @param  [type]     $orderid [description]
+     * @return [type]              [description]
+     */
     public function cancel($symbol, $orderid)
     {
         return $this->signedRequest("v3/order", ["symbol" => $symbol, "orderId" => $orderid], "DELETE");
     }
 
+    /**
+     * [orderStatus description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol  [description]
+     * @param  [type]     $orderid [description]
+     * @return [type]              [description]
+     */
     public function orderStatus($symbol, $orderid)
     {
         return $this->signedRequest("v3/order", ["symbol" => $symbol, "orderId" => $orderid]);
     }
 
+    /**
+     * [openOrders description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol [description]
+     * @return [type]             [description]
+     */
     public function openOrders($symbol)
     {
         return $this->signedRequest("v3/openOrders", ["symbol" => $symbol]);
     }
 
+    /**
+     * [orders description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol [description]
+     * @param  integer    $limit  [description]
+     * @return [type]             [description]
+     */
     public function orders($symbol, $limit = 500)
     {
         return $this->signedRequest("v3/allOrders", ["symbol" => $symbol, "limit" => $limit]);
     }
 
+    /**
+     * [trades description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol [description]
+     * @return [type]             [description]
+     */
     public function trades($symbol)
     {
         return $this->signedRequest("v3/myTrades", ["symbol" => $symbol]);
@@ -57,21 +114,47 @@ class BinanceApiClient
         return $this->priceData($this->request("v1/ticker/allPrices"));
     }
 
+    /**
+     * [bookPrices description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @return [type]     [description]
+     */
     public function bookPrices()
     {
         return $this->bookPriceData($this->request("v1/ticker/allBookTickers"));
     }
 
+    /**
+     * [account description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @return [type]     [description]
+     */
     public function account()
     {
         return $this->signedRequest("v3/account");
     }
 
+    /**
+     * [depth description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol [description]
+     * @return [type]             [description]
+     */
     public function depth($symbol)
     {
         return $this->request("v1/depth", ["symbol" => $symbol]);
     }
 
+    /**
+     * [balances description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  boolean    $priceData [description]
+     * @return [type]                [description]
+     */
     public function balances($priceData = false)
     {
         $balance = $this->signedRequest("v3/account");
@@ -81,6 +164,14 @@ class BinanceApiClient
         return $this->balanceData($balance, $priceData);
     }
 
+    /**
+     * [request description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $url    [description]
+     * @param  array      $params [description]
+     * @return [type]             [description]
+     */
     private function request($url, $params = [])
     {
         $headers[] = "User-Agent: Mozilla/4.0 (compatible; PHP Binance API)\r\n";
@@ -88,6 +179,15 @@ class BinanceApiClient
         return json_decode($this->http_request($this->base . $url . '?' . $query, $headers), true);
     }
 
+    /**
+     * [http_request description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $url     [description]
+     * @param  [type]     $headers [description]
+     * @param  array      $data    [description]
+     * @return [type]              [description]
+     */
     public function http_request($url, $headers, $data = array())
     {
         $ch = curl_init();
@@ -111,7 +211,14 @@ class BinanceApiClient
         return $content;
     }
 
-
+    /**
+     * [signedRequest description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $url    [description]
+     * @param  array      $params [description]
+     * @return [type]             [description]
+     */
     private function signedRequest($url, $params = [])
     {
         $headers[] = "User-Agent: Mozilla/4.0 (compatible; PHP Binance API)\r\nX-MBX-APIKEY: {$this->key}\r\n";
@@ -122,26 +229,53 @@ class BinanceApiClient
         return json_decode($this->http_request($endpoint, $headers), true);
     }
 
+    /**
+     * [order description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $side     [description]
+     * @param  [type]     $symbol   [description]
+     * @param  [type]     $quantity [description]
+     * @param  [type]     $price    [description]
+     * @param  string     $type     [description]
+     * @return [type]               [description]
+     */
     private function order($side, $symbol, $quantity, $price, $type = "LIMIT")
     {
         $opt = [
-            "symbol" => $symbol,
-            "side" => $side,
-            "type" => $type,
-            "price" => $price,
-            "quantity" => $quantity,
+            "symbol"      => $symbol,
+            "side"        => $side,
+            "type"        => $type,
+            "price"       => $price,
+            "quantity"    => $quantity,
             "timeInForce" => "GTC",
-            "recvWindow" => 60000
+            "recvWindow"  => 60000
         ];
         return $this->signedRequest("v3/order", $opt, "POST");
     }
 
-    //1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+    /**
+     * [candlesticks description]
+     * 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $symbol   [description]
+     * @param  string     $interval [description]
+     * @return [type]               [description]
+     */
     public function candlesticks($symbol, $interval = "5m")
     {
         return $this->request("v1/klines", ["symbol" => $symbol, "interval" => $interval]);
     }
 
+    /**
+     * [balanceData description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $array     [description]
+     * @param  boolean    $priceData [description]
+     * @return [type]                [description]
+     */
     private function balanceData($array, $priceData = false)
     {
         if ($priceData) $btc_value = 0.00;
@@ -170,20 +304,34 @@ class BinanceApiClient
         return $balances;
     }
 
+    /**
+     * [bookPriceData description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $array [description]
+     * @return [type]            [description]
+     */
     private function bookPriceData($array)
     {
         $bookprices = [];
         foreach ($array as $obj) {
             $bookprices[$obj['symbol']] = [
-                "bid" => $obj['bidPrice'],
+                "bid"  => $obj['bidPrice'],
                 "bids" => $obj['bidQty'],
-                "ask" => $obj['askPrice'],
+                "ask"  => $obj['askPrice'],
                 "asks" => $obj['askQty']
             ];
         }
         return $bookprices;
     }
 
+    /**
+     * [priceData description]
+     * @author jybtx
+     * @date   2020-03-28
+     * @param  [type]     $array [description]
+     * @return [type]            [description]
+     */
     private function priceData($array)
     {
         $prices = [];
